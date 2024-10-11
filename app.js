4093164f07407b3303b13b6d43d2a79152e6b7d2
vishnu yadav <vishnuhurda787@gmail.com>
2024-10-11 17:04:20 +0530

    project_01 (weather app)

---
+++ app.js
@@ -0,0 +1,72 @@
+import API_KEY from './apikey.js';
+async function getdata(apikey,city) {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
+
+try {
+      let response = await fetch(url);
+     let data = await response.json();
+     if (data) {
+    
+        setdata(
+        data.location.name,
+        data.current.temp_c,
+        data.current.humidity,
+        data.current.wind_kph,
+        );
+    }
+} catch (error) {
+    alert("enter valid city namer");
+    console.error(error);
+ }
+}
+function setdata(city,temp,humidity,windspeed,icon) {
+
+    document.querySelector(".city").innerHTML = city ;
+    document.querySelector(".temp").innerHTML=`${temp}°C`;
+    document.querySelector("#humidity").innerHTML =`${humidity}%` ;
+    document.querySelector("#windspeed").innerHTML = `${windspeed}Km/h`;
+    const weatherimage = document.getElementById('image');
+const weathrimgsrc = getweatherimage(temp);
+weatherimage.src = weathrimgsrc;
+ 
+function getweatherimage(tempereture) {
+    let weatherimagesrc;
+    switch (true) {
+        case (tempereture <=0 ):
+            weatherimagesrc ="./images/ice.png";
+            
+            break;
+            case (tempereture <= 0 ):
+                weatherimagesrc ="./images/moisture.png";
+                
+                break;
+                case (tempereture <=15 ):
+                    weatherimagesrc ="./images/no rain.png";
+                    
+                    break;
+                    case (tempereture <=30 ):
+                        weatherimagesrc ="./images/rain.png";
+                        
+                        break;
+    
+        default:
+            weatherimagesrc = "./images/clear.gif"
+            break;
+    }
+    return weatherimagesrc;
+}
+}
+     document.querySelector(".icon").addEventListener("click",()=>{
+    const city = document.getElementById("inputbox").value;
+if (city) {
+    getdata(API_KEY,city);
+}else {
+    alert ("enter city name");
+}
+
+});
+
+window.onload = ()=>{
+    getdata(API_KEY,"jaipur");
+};
+
